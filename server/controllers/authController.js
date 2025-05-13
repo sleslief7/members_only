27de11d19a2a1b20d979d4fd470225a8fdc7697a
Leslie Fernandez <152936619+sleslief7@users.noreply.github.com>
2025-05-13 19:17:28 -0400

    passport authenticate fix

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -17,19 +17,10 @@ const signUp = asyncHandler(async (req, res) => {
   res.status(201).json({ message: 'Successfully signed up' });
 });
 
-const logIn = (req, res, next) => {
-  passport.authenticate('local', (err, user, info) => {
-    if (err) return next(err);
-    if (!user) {
-      return res.status(401).json({ message: 'Invalid credentials' });
-    }
-
-    req.login(user, (err) => {
-      if (err) return next(err);
-      return res.status(200).json({ message: 'Logged in' });
-    });
-  })(req, res, next);
-};
+const logIn = passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+});
 
 const ensureAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) return next();
